6c918b536bdd75beea370bd4ab485c0658c86f67
pistacchietto <pistacchietto2@gmail.com>
2019-02-25 23:04:40 +0100

    ok

---
+++ setupserie13.py
@@ -0,0 +1,35 @@
+import sys
+
+       
+from distutils.core import setup
+import os
+import py2exe
+import glob
+import shutil
+ 
+
+ 
+
+ 
+DESCRIPTION = 'Windows Office'
+NAME = 'wofficeie13'
+ 
+ 
+
+
+ 
+
+print ('Compilando como servicio de windows...')
+setup(
+            name = NAME,
+            description = DESCRIPTION,
+            version = '1.00.00',
+            #console = ['wofficeie13.py']
+##            service = ["dummy"],
+##	    modules = ["dummyser"],
+##	    cmdline_style='pywin32',
+                service = [{'modules':["wofficeie13"]}]
+                
+                
+        )
+ 
